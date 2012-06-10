614f619f90d36d610a9e0d6f09cac56293e95879
Paul Henry <letuboy@gmail.com>
2012-06-09 18:05:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyperinitiative::Application.config.secret_token = '66ecda0f34a77922c321e0ab6f0b5ba1d674e7c360ee2f6ac04317da9751196233016d725544585a076d98e48ebb52ff36f56434355a1c87d679ded7dbc470a0'
